acf3755b7111e0123ef558f230bf5f9966c7f49c
evaccaro <erv230@nyu.edu>
2018-01-04 20:10:36 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,35 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = { pets: [] }, action) {
+  switch (action.type) {
+    case "ADD_PET":
+      return Object.assign({}, state, {
+        pets: [...state.pets, action.pet]
+      });
+    case "REMOVE_PET":
+      const removalIndex = state.pets.findIndex(pet => pet.id === action.id);
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets.slice(0, removalIndex),
+          ...state.pets.slice(removalIndex + 1)
+        ]
+      });
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action) {
+  state = managePets(state, action);
+  render();
 }
 
-export function render(){
-
+export function render() {
+  let holder = document.getElementById("container");
+  let petList = state.pets
+    .map(pet => {
+      return `<li>${pet.name}</li>`;
+    })
+    .join(" ");
+  holder.innerHTML = `<ul>${petList}</ul>`;
 }
