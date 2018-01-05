9274e85187682e508350fef1a38dfa6f1375d50b
Fabiano Cunha <cunhasb@aol.com>
2018-01-05 17:24:38 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,35 @@
+import React from "react";
 export let state;
 
-
-export function managePets(){
+export function managePets(state = { pets: [] }, action) {
+  switch (action.type) {
+    case "ADD_PET":
+      return { pets: [...state.pets, action.pet] };
+    case "REMOVE_PET": {
+      const filtered = state.pets.filter(pet => {
+        return pet.id !== action.id;
+      });
+      return { pets: filtered };
+    }
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action) {
+  state = managePets(state, action);
+  render();
+  return state;
 }
 
-export function render(){
-
+export function render() {
+  let container = document.getElementById("container");
+  let petsList = state.pets
+    .map(pet => {
+      return `<li>${pet.name}</li>`;
+    })
+    .join(" ");
+  console.log("petlist", petsList);
+  container.innerHTML = `<ul>${petsList}</ul>`;
+  console.log("container", container, container.innerHTML);
 }
